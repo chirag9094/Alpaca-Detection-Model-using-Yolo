c77decc8fc08c23e4878b7add48760944ca1041a
chirag9094 <82878696+chirag9094@users.noreply.github.com>
2024-06-24 11:47:33 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 def main():
     model = YOLO('yolov8n.pt')
-    results = model.train(data='config.yaml', imgsz=640, epochs=1, batch=1, name='yolov8n_v8_50e67')
+    results = model.train(data='config.yaml', imgsz=640, epochs=15, batch=1, name='yolov8n_v8_50e67')
 
 if __name__ == '__main__':
     main()
