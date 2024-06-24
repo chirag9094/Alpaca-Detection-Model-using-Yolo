2091c018dd4842b72b103fcf80595a1ca0f99857
chirag9094 <82878696+chirag9094@users.noreply.github.com>
2024-06-24 11:11:17 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+def main():
+    model = YOLO('yolov8n.pt')
+    results = model.train(data='config.yaml', imgsz=640, epochs=1, batch=1, name='yolov8n_v8_50e67')
+
+if __name__ == '__main__':
+    main()
